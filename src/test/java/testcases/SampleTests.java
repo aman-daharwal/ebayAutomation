645f87cec37718cebd10df02003dde43d6ef0500
Aman Daharwal <aman.daharwal@globallogic.com>
2018-06-20 12:48:16 +0530

    Updated sample test

--- src/test/java/testcases/SampleTests.java
+++ src/test/java/testcases/SampleTests.java
@@ -1,27 +1,30 @@
 package testcases;
 
-import commons.Log;
+import logger.Log;
 import commons.TestBase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import screens.EbayHomePage;
 
 public class SampleTests extends TestBase
 {
+    EbayHomePage homePage;
+
+    @BeforeTest
+    public void initialization() {
+       homePage = new EbayHomePage(driver);
+    }
+
     @Test
     public void test1()
     {
         Log.startTestCase("Test 1");
 
-        //WebElement element = driver.findElement(By.xpath("//input[@name='q']"));
-        WebElement element = driver.findElement(By.xpath("//*[@id='gh-ac']"));
+        homePage.enterTextInSearchBox("iPhone");
 
-        element.sendKeys("iphone");
-
-        WebElement elemnet2 = driver.findElement(By.xpath("//*[@id='gh-btn']"));
-        elemnet2.submit();
+        homePage.clickSearchButton();
 
         Log.endTestCase("End");
     }
