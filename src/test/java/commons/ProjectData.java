2f58cea1184cf355055dc3d2ec648adce7d00719
Aman Daharwal <aman.daharwal@globallogic.com>
2018-06-20 18:31:42 +0530

    Added screenshot folder path in project data

--- src/test/java/commons/ProjectData.java
+++ src/test/java/commons/ProjectData.java
@@ -6,4 +6,5 @@ public class ProjectData {
     public static String baseURL = "https://www.ebay.in/";
     public static String projectPath = new File("").getAbsolutePath();
     public static String testDataFile = new File("").getAbsolutePath() + "//src//test//resources//TestData//TestData.properties";
+    public static String screenshotFolderPath = new File("").getAbsolutePath() + "//test-screenshots";
 }
