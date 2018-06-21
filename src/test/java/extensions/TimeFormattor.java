1388768bbe8ab9901f9727516986055ac60f55f1
Aman Daharwal <aman.daharwal@globallogic.com>
2018-06-21 15:22:34 +0530

    added new extension class to format time

---
+++ src/test/java/extensions/TimeFormattor.java
@@ -0,0 +1,17 @@
+package extensions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeFormattor {
+
+    public static String getCurrentTime()
+    {
+        return new SimpleDateFormat("mm-HH-ss").format(new Date());
+    }
+
+    public static  String getCurrentDate()
+    {
+        return new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+    }
+}
