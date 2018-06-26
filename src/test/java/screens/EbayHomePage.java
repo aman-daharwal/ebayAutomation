59dff7372eaec99ed7422bf1de7101be6a18f76a
Aman Daharwal <aman.daharwal@globallogic.com>
2018-06-26 15:36:49 +0530

    added methods for navigation bars

--- src/test/java/screens/EbayHomePage.java
+++ src/test/java/screens/EbayHomePage.java
@@ -1,5 +1,6 @@
 package screens;
 
+import extensions.Requirements;
 import extensions.UIScreenActions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TakesScreenshot;
@@ -10,6 +11,7 @@ import org.openqa.selenium.support.PageFactory;
 import services.logger.Log;
 import services.screenshot.Screenshot;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,5 +32,32 @@ public class EbayHomePage extends EbayPageCommons{
         short numberOfTiles = (short) tiles.size();
         Log.info("Number of Tiles inside Home Page : " + numberOfTiles);
         return numberOfTiles;
+    }
+
+    private List<String> navigationBarElements = Arrays.asList("Deals","Mobile & Accessories","Laptops & Tablets","Fashion","Cameras","Televisions","Health & Beauty","All Electronics");
+
+    public void mouseHoverNavigationBarElement(String elementName)
+    {
+        Log.info("Mouse hovering over element "+elementName);
+        if(!navigationBarElements.contains(elementName))
+        {
+            Log.error("No such element available in navigation bar");
+            return;
+        }
+
+        WebElement element = driver.findElement(By.xpath("//*[@role='listitem']/a[text()='"+elementName+"']"));
+
+        new UIScreenActions(driver);
+        UIScreenActions.mouseHoverOverElement(element);
+
+        Requirements.waitInSeconds(2);
+    }
+
+    public void selectElementUnderNavigationBar(String elementName)
+    {
+        Log.info("Clicking on element "+elementName+" under naviagtion bar");
+        WebElement element = driver.findElement(By.xpath("//*[@role='listitem']/a[text()='Mobile & Accessories']//following-sibling::div[@class='sub']/div[@class='sub-cats']//li/a[contains(@title,'"+elementName+"')]"));
+
+        element.click();
     }
 }
