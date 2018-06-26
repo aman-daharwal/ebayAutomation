9056151ae82c9f294ffeab55e6390d9f64e39eee
Aman Daharwal <aman.daharwal@globallogic.com>
2018-06-26 15:39:08 +0530

    added new extension class for test requirement

---
+++ src/test/java/extensions/Requirements.java
@@ -0,0 +1,18 @@
+package extensions;
+
+import services.logger.Log;
+
+import java.util.concurrent.TimeUnit;
+
+public class Requirements {
+
+    public static void waitInSeconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        }
+        catch(InterruptedException ieException)
+        {
+            Log.error("Exception in waiting for "+seconds+" seconds \n"+ieException.getLocalizedMessage());
+        }
+    }
+}
