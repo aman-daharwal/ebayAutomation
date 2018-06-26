9f82dcf24a1603a9358d41707407c2911ce09e74
Aman Daharwal <aman.daharwal@globallogic.com>
2018-06-26 15:37:15 +0530

    added test for navigation bar

--- src/test/java/testcases/EbayItemCategoryTests.java
+++ src/test/java/testcases/EbayItemCategoryTests.java
@@ -1,6 +1,5 @@
 package testcases;
 
-import com.sun.org.glassfish.gmbal.Description;
 import commons.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -32,5 +31,18 @@ public class EbayItemCategoryTests extends TestBase {
 
         Assert.assertEquals(categoryPage.getCategoryTitle().trim(),getValueFromTestData("Category").trim(),"Category select mismatch, getting "
                 +categoryPage.getCategoryTitle()+" ,instead of "+getValueFromTestData("Category"));
+    }
+
+    @Test(description = "Test the navigation bar of ebay home page")
+    public void NavigationBarTest()
+    {
+        Log.startTestCase("Select element from Navigation Bar");
+
+        homePage.mouseHoverNavigationBarElement(getValueFromTestData("NavCategory"));
+
+        homePage.selectElementUnderNavigationBar(getValueFromTestData("SubCategory"));
+
+        Assert.assertEquals(getValueFromTestData("SubCategory"), categoryPage.getSubCategoryTitle(),"Sub Category select mismatch, getting "
+                +categoryPage.getSubCategoryTitle()+" ,instead of "+getValueFromTestData("SubCategory"));
     }
 }
