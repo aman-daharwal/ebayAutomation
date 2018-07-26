40650694ed688d7d84375a9225c1fe9f8ddaee23
Aman Daharwal <aman.daharwal@globallogic.com>
2018-07-26 17:58:49 +0530

    Added new method to wait for screen loading

--- src/test/java/core/commonExtensions/Requirements.java
+++ src/test/java/core/commonExtensions/Requirements.java
@@ -1,6 +1,11 @@
 package core.commonExtensions;
 
 import core.services.logger.Log;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,10 +14,20 @@ public class Requirements {
     public static void waitInSeconds(int seconds) {
         try {
             TimeUnit.SECONDS.sleep(seconds);
-        }
-        catch(InterruptedException ieException)
-        {
-            Log.error("Exception in waiting for "+seconds+" seconds \n"+ieException.getLocalizedMessage());
+        } catch (InterruptedException ieException) {
+            Log.error("Exception in waiting for " + seconds + " seconds \n" + ieException.getLocalizedMessage());
         }
     }
+
+    public static void waitForScreenToLoad(AppiumDriver driver,String pageTitle, int seconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, seconds);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(getElement(pageTitle)));
+    }
+
+    public static By getElement(String elementText)
+    {
+        return By.xpath("//*[@text='"+elementText+"']");
+    }
 }
+
