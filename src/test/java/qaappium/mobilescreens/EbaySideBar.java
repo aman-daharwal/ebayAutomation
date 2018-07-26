6b08a596017826623e93be12e43173a5fd6328d9
Aman Daharwal <aman.daharwal@globallogic.com>
2018-07-26 17:59:26 +0530

    added methods for user sign out and signed user name

--- src/test/java/qaappium/mobilescreens/EbaySideBar.java
+++ src/test/java/qaappium/mobilescreens/EbaySideBar.java
@@ -3,6 +3,8 @@ package qaappium.mobilescreens;
 import core.services.logger.Log;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import qaappium.mobileEnums.EbaySideBarItems;
 
@@ -20,5 +22,28 @@ public class EbaySideBar{
     {
         Log.info("Clicking "+item.getValue());
         driver.findElement(By.xpath("//*[@text='"+item.getValue()+"']")).click();
+    }
+
+    @FindBy(id = "com.ebay.mobile:id/textview_sign_in_status")
+    WebElement signedUser;
+
+    public String getSignedUser()
+    {
+        Log.info("Signed User's Username :"+signedUser.getText());
+        return signedUser.getText();
+    }
+
+    @FindBy(xpath = "//*[@text='Sign out']")
+    WebElement signOutButton;
+
+    public void userSignOut()
+    {
+        Log.info("Clicking sign out and signing out user");
+        signedUser.click();
+
+        if(signOutButton.isDisplayed())
+            signOutButton.click();
+        else
+            Log.error("Sign Out button is not visible");
     }
 }
